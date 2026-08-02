991769514967d5ae6559e5e9c061579449930f64
vedant <vedantyewale37@gmail.com>
2026-08-03 01:35:53 +0530

    Fix user model import

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 /* ==========================================
    REGISTER USER
